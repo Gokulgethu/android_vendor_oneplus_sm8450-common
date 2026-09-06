062a0b50f7e934395dff34d0be39d10cd207ef45
The LineageOS Project <dev@lineageos.org>
2026-09-06 08:45:20 +0000

    sm8475-common: Add all real proprietary binary blobs and sm8475-common-vendor.mk

---
+++ sm8475-common-vendor.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2023-2026 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product-if-exists, vendor/oneplus/sm8450-common/sm8450-common-vendor.mk)
+$(call inherit-product-if-exists, vendor/oneplus/sm8475-common/sm8450-common-vendor.mk)
